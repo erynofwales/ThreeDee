6434ef1cad4fb0ba78880850b9eb17333a5c1502
Eryn Wells <eryn@erynwells.me>
2015-10-28 20:37:19 -0700

    Basic Math target/project for vector math

---
+++ Math/Math/Math.h
@@ -0,0 +1,19 @@
+//
+//  Math.h
+//  Math
+//
+//  Created by Eryn Wells on 10/28/15.
+//  Copyright © 2015 Eryn Wells. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Math.
+FOUNDATION_EXPORT double MathVersionNumber;
+
+//! Project version string for Math.
+FOUNDATION_EXPORT const unsigned char MathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Math/PublicHeader.h>
+
+
