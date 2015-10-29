84f7d6bc2cee414955ecd0291916543d50abae0b
Eryn Wells <eryn@erynwells.me>
2015-10-28 20:38:46 -0700

    Add empty COLLADA target/project to the workspace

---
+++ COLLADA/COLLADA/COLLADA.h
@@ -0,0 +1,19 @@
+//
+//  COLLADA.h
+//  COLLADA
+//
+//  Created by Eryn Wells on 10/28/15.
+//  Copyright © 2015 Eryn Wells. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for COLLADA.
+FOUNDATION_EXPORT double COLLADAVersionNumber;
+
+//! Project version string for COLLADA.
+FOUNDATION_EXPORT const unsigned char COLLADAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <COLLADA/PublicHeader.h>
+
+
