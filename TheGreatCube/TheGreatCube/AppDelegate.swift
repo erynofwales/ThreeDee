7f27ca7bc6ed2eaa3e5d16d82354c5410282591b
Eryn Wells <eryn@erynwells.me>
2015-11-14 22:17:24 -0800

    [TheGreatCube] A port of my HelloGLTriangle project to Swift and my libraries

---
+++ TheGreatCube/TheGreatCube/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TheGreatCube
+//
+//  Created by Eryn Wells on 11/14/15.
+//  Copyright © 2015 Eryn Wells. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TheGreatCube/TheGreatCube/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TheGreatCube
+//
+//  Created by Eryn Wells on 11/14/15.
+//  Copyright © 2015 Eryn Wells. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
